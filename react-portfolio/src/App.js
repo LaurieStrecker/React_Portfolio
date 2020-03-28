af047ff26da1355f5ba8430b41b8f9d6d10d338d
LahRee <Laurie.Strecker@gmail.com>
2020-03-28 16:41:04 -0700

    added browsr, router pkgs, and scripts with navigation

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './box-black.png';
+
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+import Projects from './Projects';
+import Articles from './Articles';
+import About from './About';
+
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Route exact path="/" component={Projects} />
+    <Route path="/articles" component={Articles} />
+    <Route path="/about" component={About} />
+
+    
+     <div className="navigation">
+       <img src={logo} className="App-logo" alt="logo" />
+       <div className="navigation-sub">
+
+       <Link to="/" className="item">Projects</Link>
+       <Link to="/articles" className="item">Artilces</Link>
+       <Link to="/about" className="item">About</Link>
+       </div>
+     </div>
     </div>
+    </BrowserRouter>
   );
 }
 
